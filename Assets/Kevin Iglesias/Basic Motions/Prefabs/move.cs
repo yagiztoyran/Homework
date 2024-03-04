0303acb899bc238f148a0502f1722bba1f369e5c
toyranyagiz <toyranyagiz@gmail.com>
2024-03-04 19:09:26 +0300

    Initial commit

---
+++ Assets/Kevin Iglesias/Basic Motions/Prefabs/move.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public int speed = 8;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+    }
+}
